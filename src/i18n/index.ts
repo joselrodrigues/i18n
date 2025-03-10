72e50ea213450168c6008665c4f0633a9ad3251e
joselrodrigues <ing.joserodrigues@gmail.com>
2025-03-10 11:25:15 -0300

    fix: update package version

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,2 +1,3 @@
 export { default as useInstanceTranslation } from './useInstanceTranslation';
 export { default as i18nService } from './i18nService';
+export { I18nextProvider } from 'react-i18next';
